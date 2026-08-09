c39c5b4e16207be5671d2b4e17a3a1be9ed241c9
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-09 19:13:22 +0530

    RoboShop Project

--- shipping.sh
+++ shipping.sh
@@ -1,63 +1,17 @@
 #!/bin/bash
 
-START_TIME=$(date +%s)
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+source ./common.sh
+app_name=shipping
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1
-else
-    echo "You are running with the root access" | tee -a $LOG_FILE
-fi
+CHECK_ROOT
 
 echo "Please enter root password to setup"
 read -s MYSQL_ROOT_PASSWORD
 
-# validate function takes inputs exit status as $1 and which command they try to install as $2
-VALIDATE(){
-        if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+APP_SETUP
 
 dnf install maven -y &>>$LOG_FILE
 VALIDATE $? "Installing Maven and Java"
-
-id roboshop &>>$LOG_FILE
-if [ $? -ne  0 ]
-then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
-    VALIDATE $? "Creating roboshop system user"
-else
-    echo -e "System user roboshop is already created ... $Y SKIPPING $N"
-fi
-
-mkdir -p /app &>>$LOG_FILE
-VALIDATE $? "Creating app directory"
-
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading shipping"
-
-rm -rf /app/* &>>$LOG_FILE
-cd /app 
-unzip /tmp/shipping.zip &>>$LOG_FILE
-VALIDATE $? "unzipping shipping"
 
 mvn clean package &>>$LOG_FILE
 VALIDATE $? "Packaging the shipping application"
