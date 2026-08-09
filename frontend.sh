10e624265159d0a3f962a188df192fde298d4983
Talluri Manoj Kumar <manojtalluri5087@gmail.com>
2026-08-09 18:39:20 +0530

    RoboShop Project

--- frontend.sh
+++ frontend.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
+source ./common.sh
+app_name=frontend
 
-mkdir -p $LOGS_FOLDER
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-    exit 1
-else
-    echo "You are running with the root access" | tee -a $LOG_FILE
-fi
-
-# validate function takes inputs exit status as $1 and which command they try to install as $2
-VALIDATE(){
-        if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-    else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+CHECK_ROOT
 
 dnf module disable nginx -y &>>$LOG_FILE
 VALIDATE $? "Disabling Default Nginx"
@@ -63,3 +36,5 @@ VALIDATE $? "Copying nginx.conf"
 
 systemctl restart nginx
 VALIDATE $? "Restarting nginx"
+
+PRINT_TIME
